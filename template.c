d914e490a790a7e1469a200186fb4b4b106990ba
dougrenaux <douglasrenaux@utfpr.edu.br>
2023-09-16 16:53:40 -0300

    included templates

---
+++ template.c
@@ -0,0 +1,79 @@
+/*__________________________________________________________________________________
+|       Disciplina de Sistemas Embarcados - 2023-2
+|       Prof. Douglas Renaux
+| __________________________________________________________________________________
+|
+|		Lab x
+| __________________________________________________________________________________
+*/
+
+/**
+ * @file     insert filename here
+ * @author   insert authors names here
+ * @brief    Solution to Lab x ELEW32 - UTFPR. \n 
+ *           ...
+ * @version  
+ * @date     
+ ******************************************************************************/
+
+/*------------------------------------------------------------------------------
+ *
+ *      File includes
+ *
+ *------------------------------------------------------------------------------*/
+#include <stdbool.h>
+#include <stdint.h>
+#include <stdarg.h>
+#include "inc/hw_ints.h"
+#include "inc/hw_memmap.h"
+#include "inc/hw_types.h"
+#include "driverlib/debug.h"
+#include "driverlib/fpu.h"
+#include "driverlib/gpio.h"
+#include "driverlib/interrupt.h"
+#include "driverlib/pin_map.h"
+#include "driverlib/rom.h"
+#include "driverlib/rom_map.h"
+#include "driverlib/sysctl.h"
+#include "driverlib/uart.h"
+#include "drivers/buttons.h"
+#include "utils/uartstdio.h"
+#include "driverlib/timer.h"
+#include "template.h"
+
+/*------------------------------------------------------------------------------
+ *
+ *      Typedefs and constants
+ *
+ *------------------------------------------------------------------------------*/
+
+/*------------------------------------------------------------------------------
+ *
+ *      Global vars
+ *
+ *------------------------------------------------------------------------------*/
+
+
+
+/*------------------------------------------------------------------------------
+ *
+ *      File scope vars (static)
+ *
+ *------------------------------------------------------------------------------*/
+
+/*------------------------------------------------------------------------------
+ *
+ *      Functions
+ *
+ *------------------------------------------------------------------------------*/
+/**
+ * Main function.
+ *
+ * @returns int    - 
+ */
+int main( )
+{
+
+ 
+    return 0;
+}
